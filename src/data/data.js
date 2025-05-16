42fa46eae672d8859d0dedd408a6621c2819ca15
StasGil <stanislav.gill@jadexconsulting.com>
2025-05-16 16:36:39 +0200

    a

---
+++ src/data/data.js
@@ -0,0 +1,75 @@
+import samsung from "../img/samsung.jpeg";
+import ultra from "../img/ultra.jpeg";
+import vesta from "../img/vesta.jpeg";
+import dom1 from "../img/dom1.jpeg";
+import niva from "../img/niva.jpeg";
+import dom2 from "../img/dom2.jpeg";
+import dom3 from "../img/dom3.jpeg";
+import dom4 from "../img/dom4.jpeg";
+
+export const products = [
+  {
+    image: samsung,
+    title: "Смартфон Galaxy S25",
+    description: "Современный смартфон с ярким экраном и мощной батареей.",
+    phone: "+7 (900) 123-45-67",
+    filter: "Техника",
+    id: 1,
+  },
+  {
+    image: vesta,
+    title: "Лада Веста",
+    description: "Лада веста очень хорошее состояние.",
+    phone: "+7 (900) 234-56-78",
+    filter: "Транспорт",
+    id: 2,
+  },
+  {
+    image: dom1,
+    title: "Дом кирпичный",
+    description: "Дом на улице Пушкина.",
+    phone: "+7 (900) 345-67-89",
+    filter: "Недвижимость",
+    id: 3,
+  },
+  {
+    image: niva,
+    title: "Нива",
+    description: "Классная Нива.",
+    phone: "+7 (900) 456-78-90",
+    filter: "Транспорт",
+    id: 4,
+  },
+  {
+    image: ultra,
+    title: "Ноутбук UltraBook X",
+    description: "Легкий, мощный и долго работает от батареи.",
+    phone: "+7 (900) 456-78-90",
+    filter: "Техника",
+    id: 5,
+  },
+  {
+    image: dom2,
+    title: "Дом из самана",
+    description: "Очень уютный.",
+    phone: "+7 (900) 456-78-90",
+    filter: "Недвижимость",
+    id: 6,
+  },
+  {
+    image: dom3,
+    title: "Дом из самана",
+    description: "Очень уютный.",
+    phone: "+7 (900) 456-78-90",
+    filter: "Недвижимость",
+    id: 7,
+  },
+  {
+    image: dom4,
+    title: "Дом из самана",
+    description: "Очень уютный.",
+    phone: "+7 (900) 456-78-90",
+    filter: "Недвижимость",
+    id: 8,
+  },
+];
